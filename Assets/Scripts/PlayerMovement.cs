7d9d730c5a3ce7b54ca52ff6fee4e51fc1f17007
salilniem <salil.niem@bumail.net>
2026-04-21 19:38:11 +0700

    player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    float move;
+    public float JumpForce;
+    public bool isJumping;
+
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+        rb2d.linearVelocity = new Vector2(move * Speed, rb2d.linearVelocity.y);
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, JumpForce));
+            Debug.Log("Jump");
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
+    }
+}
